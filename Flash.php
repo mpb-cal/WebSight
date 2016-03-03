f4ef46bd9d1fc6f28dff3405926191aa0fd8e4b9
MPB <ytfkg3@gmail.com>
2016-03-03 06:02:05 +0000

    added file

---
+++ Flash.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace WebSight;
+
+require_once 'Session.php';
+
+// SV_ = session variable
+define( 'SV_USER_MESSAGE', 'userMessage' );
+
+class Flash
+{
+	private static $session = null;
+
+	public static function init( \WebSight\Session $session )
+	{
+		self::$session = $session;
+	}
+
+
+	public static function userMessage( $msg )
+	{
+		self::$session->set( SV_USER_MESSAGE, $msg );
+	}
+
+
+	public static function addToUserMessage( $msg )
+	{
+		self::$session->set( SV_USER_MESSAGE, sessionVar( SV_USER_MESSAGE ) . $msg . br() );
+	}
+
+
+	// this also clears the message
+	public static function getUserMessage()
+	{
+		$message = self::$session->get( SV_USER_MESSAGE );
+		self::$session->set( SV_USER_MESSAGE, '' );
+		return $message;
+	}
+}
+
+
