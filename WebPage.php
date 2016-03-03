b1a4b087ce6cc928b3694ff62543d55d2052aeb7
MPB <ytfkg3@gmail.com>
2016-03-03 19:51:09 +0000

    created head and body methods

--- WebPage.php
+++ WebPage.php
@@ -4,27 +4,55 @@ namespace WebSight;
 
 class WebPage
 {
-	private $html = null;
+	private $body = '';
+	private $bodyAtts = '';
+	private $head = '';
 
 	public function __construct()
 	{
-		$this->html =
-			'<!DOCTYPE html>' . NL
-		;
+		$this->addToHead( meta( '', "charset='UTF-8'" ) . NL );
 	}
 
 
 	public function output()
 	{
 		print
-			$this->html
+			'<!DOCTYPE html>' . NL
+			. html(
+				head( $this->head )
+				. body( $this->body, $this->bodyAtts )
+			)
 		;
 	}
 
 
-	public function add( $html = '' )
+	public function addToBody( $html = '' )
 	{
-		$this->html .= $html;
+		$this->body .= $html;
+	}
+
+
+	public function addBodyAtts( $atts = '' )
+	{
+		$this->bodyAtts .= $atts;
+	}
+
+
+	public function addToHead( $html = '' )
+	{
+		$this->head .= $html;
+	}
+
+
+	public function addStyleSheet( $href = '' )
+	{
+		$this->addToHead(
+			link_(
+				'',
+				"rel='stylesheet' href='$href' type='text/css' media='all'"
+			)
+			. NL
+		);
 	}
 }
 
